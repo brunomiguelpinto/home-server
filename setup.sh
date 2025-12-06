20d4bd108aacd25bd1bf396d386d0e8901a8a3ec
miguelp <darkymiguel@gmail.com>
2025-12-06 19:32:50 +0000

    added gitignore

--- setup.sh
+++ setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Check if Vim is installed and install if not
+check_vim() {
+    if [ -x "$(command -v vim)" ]; then
+        echo "Vim is already installed."
+    else
+        echo "Installing Vim..."
+        sudo apt-get install -y vim
+    fi
+}
+
+
+mkdir -p /home/miguelpinto/Desktop/home-server/logs
 echo "===== Adding update.sh to crontab ====="
 
 CRON_JOB="0 3 * * * /home/miguelpinto/Desktop/home-server/scripts/update.sh >> /home/miguelpinto/Desktop/home-server/logs/update.log 2>&1"
@@ -9,3 +21,5 @@ CRON_JOB="0 3 * * * /home/miguelpinto/Desktop/home-server/scripts/update.sh >> /
 
 echo "===== DONE! ====="
 echo "Reboot recommended: sudo reboot"
+
+check_vim             # Install Vim if not installed
