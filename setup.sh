4873c74e788faf3523cd52ea1cea09c4db874913
miguelp <darkymiguel@gmail.com>
2025-12-06 19:25:11 +0000

    added gitignore

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 echo "===== Adding update.sh to crontab ====="
 
-CRON_JOB="0 3 * * * /home/miguelpinto/Desktop/home-server/scripts/update.sh >> /home/miguelpinto/Desktop/update.log 2>&1"
+CRON_JOB="0 3 * * * /home/miguelpinto/Desktop/home-server/scripts/update.sh >> /home/miguelpinto/Desktop/home-server/logs/update.log 2>&1"
 
 (crontab -l 2>/dev/null | grep -F "$CRON_JOB") || \
 ( crontab -l 2>/dev/null; echo "$CRON_JOB" ) | crontab -
